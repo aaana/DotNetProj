459dbdf4512507834782b23f78d24edccaadf5f4
Anna Huang <593880978@qq.com>
2016-06-12 14:34:38 +0800

    Initial Commit

---
+++ Weike/Weike/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Weike.Startup))]
+namespace Weike
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
