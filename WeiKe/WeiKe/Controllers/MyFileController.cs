5d82ec77744f3b13782d68db79ed90d17fb60a85
Anna Huang <593880978@qq.com>
2016-06-23 02:42:04 +0800

    上传下载文件

---
+++ WeiKe/WeiKe/Controllers/MyFileController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeiKe.Models;
+
+namespace WeiKe.Controllers
+{
+    public static class Helper
+    {
+        public static bool HasFile(this HttpPostedFileBase file)
+        {
+            return (file != null && file.ContentLength > 0) ? true : false;
+        }
+    }
+    public class MyFileController : Controller
+    {
+        // GET: File
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Upload()
+        {
+            foreach (string upload in Request.Files)
+            {
+                if (!Request.Files[upload].HasFile()) continue;
+                string mimetype = Request.Files[upload].ContentType;
+                string path = AppDomain.CurrentDomain.BaseDirectory + "uploads/";
+                string filename = Path.GetFileName(Request.Files[upload].FileName);
+                Request.Files[upload].SaveAs(Path.Combine(path, filename));
+                MyFileDB.Insert(new MyFile(0, filename, mimetype, path));
+            }
+            return View();
+        }
+
+        public FilePathResult GetFileFromDisk()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "uploads/";
+            string fileName = "捕获1.PNG";
+            return File(path + fileName, "text/plain", "捕获1.PNG");
+        }
+    }
+
+
+    }
---
+++ WeiKe/WeiKe/Models/MyFileDB.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeiKe.Models
+{
+    public class MyFileDB
+    {
+        public static void Insert(MyFile file)
+        {
+            string sql = "insert into file VALUES (" + file.id + ",'" + file.fileName + "','"+file.mimeType+"','" + file.fileUrl + "')";
+            MySqlConnection conn = Connection.getMySqlCon();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = sql;
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
