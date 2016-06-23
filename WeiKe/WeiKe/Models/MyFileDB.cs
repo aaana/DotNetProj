083e2889754acfca94384978d9eee961c657984d
Anna Huang <593880978@qq.com>
2016-06-23 15:40:56 +0800

    ...

--- WeiKe/WeiKe/Models/MyFileDB.cs
+++ WeiKe/WeiKe/Models/MyFileDB.cs
@@ -18,5 +18,24 @@ namespace WeiKe.Models
             cmd.ExecuteNonQuery();
             conn.Close();
         }
+
+        public static MyFile FindByWeikeId(int weikeId)
+        {
+            MyFile result = null;
+            string sql = "select * from file where weike_id = @weike_id";
+            MySqlConnection conn = Connection.getMySqlCon();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = sql;
+            cmd.Parameters.AddWithValue("@weike_id", weikeId);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read()) {
+                result = new MyFile((int)reader["id"], reader.GetString("fileName"), reader.GetString("mimeType"), reader.GetString("fileUrl"), (int)reader["weike_id"]);
+            }
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            return result;
+        }
     }
 }
